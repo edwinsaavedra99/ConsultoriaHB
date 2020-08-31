16d8ddea83b6d879e5a4978f17c824c806ea0e83
Luis Angel Lorenzo Quilla <louysotopo123@gmail.com>
2020-08-30 22:57:59 -0500

    navbar reponsive

--- consultoriaHB/src/app/components/client/static/header/header.component.ts
+++ consultoriaHB/src/app/components/client/static/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  @HostListener('window:scroll',['$event'])
+  doSomethingOnWindowScroll(e){
+    let element = document.querySelector('.header');
+    let elem = document.getElementById('#menu-btn');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('asas');
+    } else {
+      //if(document.getElementById('#menu-btn').getAttribute('checked')){
+        //element.classList.add('asas');
+      //}else{
+      element.classList.remove('asas');
+      //}
+    }
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
