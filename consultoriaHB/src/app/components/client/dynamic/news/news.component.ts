8ac21162ff036fa77b00b5b1413414ced6011f02
Elmer Davila <40619853+elmerdavila@users.noreply.github.com>
2020-09-04 20:28:49 -0500

    Seccion Noticias - Conectado BD

--- consultoriaHB/src/app/components/client/dynamic/news/news.component.ts
+++ consultoriaHB/src/app/components/client/dynamic/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService} from '../../../../services/news/news.service';
+import { New } from '../../../../models/new';
 
 @Component({
   selector: 'app-news',
@@ -9,91 +11,28 @@ export class NewsComponent implements OnInit {
 
   totalRecords: String;
   page: Number= 1;
-  noticias:any[];
+  noticias:New[]=[];
 
-  constructor() { 
-    this.noticias=[
-      {
-        titulo:'Derecho Ambiental',
-        imagenUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058',
-        contenido:'Podemos crear la identidad corporativa de tu empresa. Diseño , Maquetación de folletos, Catálogos, Papelería y mucho más.' },
-      {
-        titulo:'Derecho Tributario y Comercial',
-        imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-        contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-      {
-        titulo:'Derecho Tributario',
-        imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-        contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-      {
-        titulo:'Derecho Inmoviliario',
-        imagenUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058',
-        contenido:'Podemos crear la identidad corporativa de tu empresa. Diseño , Maquetación de folletos, Catálogos, Papelería y mucho más.' },
-      {
-        titulo:'Derecho Corporativo',
-        imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-        contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-      {
-        titulo:'Derecho Regional',
-        imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-        contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-      {
-        titulo:'Derecho Inmoviliario',
-        imagenUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058',
-        contenido:'Podemos crear la identidad corporativa de tu empresa. Diseño , Maquetación de folletos, Catálogos, Papelería y mucho más.' },
-      {
-        titulo:'Derecho Corporativo',
-        imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-        contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-      {
-        titulo:'Derecho Regional',
-        imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-        contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-         {
-        titulo:'Derecho Corporativo',
-        imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-        contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-      {
-        titulo:'Derecho Regional',
-        imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-        contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-      {
-        titulo:'Derecho Inmoviliario',
-        imagenUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058',
-        contenido:'Podemos crear la identidad corporativa de tu empresa. Diseño , Maquetación de folletos, Catálogos, Papelería y mucho más.' },
-      {
-        titulo:'Derecho Corporativo',
-        imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-        contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-      {
-        titulo:'Derecho Regional',
-        imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-        contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-        {
-          titulo:'Derecho Corporativo',
-          imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-          contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-        {
-          titulo:'Derecho Regional',
-          imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-          contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' },
-        {
-          titulo:'Derecho Inmoviliario',
-          imagenUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ9DMNFHxwZcfPXJrJeBMITxPMP3FMZk_ixXzTfzt4G_C-G058',
-          contenido:'Podemos crear la identidad corporativa de tu empresa. Diseño , Maquetación de folletos, Catálogos, Papelería y mucho más.' },
-        {
-          titulo:'Derecho Corporativo',
-          imagenUrl:'https://colmayorbolivar.edu.co/blog/wp-content/uploads/2017/06/imagen-administracion.jpg',
-          contenido:'Nosotros creamos y optimizamos tus perfiles en las Redes Sociales. Importantes para que tu presencia online sea completa.' },
-        {
-          titulo:'Derecho Regional',
-          imagenUrl:'https://image.freepik.com/foto-gratis/desarrollo-programadores-desarrollo-tecnologias-diseno-codificacion-sitios-web_18497-1090.jpg',
-          contenido:'Creamos tu página web utilizando las últimas tecnologías disponibles. Una Web adaptativa a tu móvil o Tablet y con un gestor de contenido' }
-        ];
+  listNew: New[] = [];
+
+  constructor(private newService: NewsService) { 
+    this.getAreaList();
+    this.noticias=this.listNew;
+    this.listNew.length = 12;
   }
-
   ngOnInit(): void {
   }
+  getAreaList() {
+    this.newService.getNewList().snapshotChanges().subscribe(res=>{
+      this.listNew.length = 0;
+      res.forEach( t=>{
+        const new1 = t.payload.toJSON();
+        this.listNew.push(new1 as New)
+
+      })
+    });
+  }
+  
 
 
 
