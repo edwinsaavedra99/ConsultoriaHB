cf5466eefd68b56d73759577c0c49ae2af35a70b
Nelson-Fuentes <40514220+Nelson-Fuentes@users.noreply.github.com>
2020-09-08 17:07:34 -0500

    Guard for every module admin

--- consultoriaHB/src/app/components/admin/dynamic/dynamic.routing.ts
+++ consultoriaHB/src/app/components/admin/dynamic/dynamic.routing.ts
@@ -5,17 +5,19 @@ import { RequestsComponent } from './requests/requests.component';
 import { NewsComponent } from './news/news.component'
 import { ContactComponent} from './contact/contact.component'
 import { LegalAreasComponent } from './legal-areas/legal-areas.component';
-//import {AdminGuard} from 'src/app/Services/admin/admin.guard';
+import { AuthGuard } from '../../../guard/auth.guard';
+
 import { NgModule } from '@angular/core';
 import { from } from 'rxjs';
+//    canActivate: [AuthGuard]
 
 export const DynamicRoutes: Routes =[
-    { path: 'info',             component: InfoComponent }, 
-    { path: 'contact',             component: ContactComponent },
-    { path: 'legal-areas',             component: LegalAreasComponent },
-    { path: 'news',             component: NewsComponent },
-    { path: 'requests',             component: RequestsComponent },
-    { path: 'users',             component: UsersComponent },
+    { path: 'info',             component: InfoComponent, canActivate: [AuthGuard] }, 
+    { path: 'contact',             component: ContactComponent, canActivate: [AuthGuard] },
+    { path: 'legal-areas',             component: LegalAreasComponent, canActivate: [AuthGuard] },
+    { path: 'news',             component: NewsComponent, canActivate: [AuthGuard] },
+    { path: 'requests',             component: RequestsComponent, canActivate: [AuthGuard] },
+    { path: 'users',             component: UsersComponent, canActivate: [AuthGuard] },
     //{ path: 'login',          component: LoginComponent },
     //{ path: 'perfil-client',         component: PerfilClientComponent,canActivate:[AdminGuard]}
 ];
