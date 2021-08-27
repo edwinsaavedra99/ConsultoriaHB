4cd12ea7cbe12f0fc03efb89745a60335223bf23
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2021-08-27 05:57:48 -0500

    form popup right

--- consultoriaHB/src/app/components/client/static/contac/contac.component.ts
+++ consultoriaHB/src/app/components/client/static/contac/contac.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-contac',
@@ -6,10 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contac.component.css']
 })
 export class ContacComponent implements OnInit {
+  
+  dataForm: any;
+  email_value:string;
+  number_value:string;
+  name_value:string;
+  surname_value:string;
+  description_value:string;
+  activar: boolean = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder : FormBuilder
+    )
+  { }
 
   ngOnInit() {
+    this.dataForm = this.formBuilder.group({
+      email : ['', {
+        validators: [
+          Validators.email,
+          Validators.required
+        ]
+      }],
+      number :[''],
+      name : ['', {
+        validators: [
+          Validators.required
+        ]
+      }],
+      surname : ['', {
+        validators: [
+          Validators.required
+        ]
+      }],
+      description : ['', {
+        validators: [
+          Validators.required
+        ]
+      }],
+    });
   }
 
+  email(){
+    return this.dataForm.get('email');
+  }
+  number(){
+    return this.dataForm.get('number')
+  }
+  name(){
+    return this.dataForm.get('name')
+  }
+  surname(){
+    return this.dataForm.get('surname')
+  }
+  description(){
+    return this.dataForm.get('description')
+  }
+
+
+  activarEmergente() {
+    this.activar = true;
+  }
+  apagarEmergente() {
+    this.activar = false;
+  }
+  submit(){}
 }
